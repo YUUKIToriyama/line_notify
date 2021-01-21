770099fba12a05051536f165687814a48bbb3b33
YUUKIToriyama <ytoriyama@torisfactory.com>
2021-01-21 19:55:39 +0900

    meowを導入し、CLIから簡単に利用できるようにした

--- cli.js
+++ cli.js
@@ -1,9 +1,32 @@
 #!/usr/bin/env node
+const meow = require("meow");
 const lineNotify = require("./src/lineNotify");
 const accessToken = require("./src/accessToken");
 
-const args = process.argv.slice(2);
+const cli = meow(
+	`
+	Usage
+		$node cli.js <input>
+	Options
+		--message, -m	message
+		--image, -i	image url
+	Examples
+		$node cli.js -m "Hello, my favarite photo for you!" -i "https://upload.wikimedia.org/wikipedia/ja/thumb/6/61/Wataraseriver.jpg/300px-Wataraseriver.jpg"
+	`, {
+	flags: {
+		message: {
+			type: "string",
+			alias: "m",
+			default: "Hello!"
+		},
+		image: {
+			type: "string",
+			alias: "i"
+		}
+	}
+}
+);
 
 const notify = new lineNotify(accessToken);
-notify.setRequest(args[0], args[1]);
+notify.setRequest(cli.flags.message, cli.flags.image);
 notify.getResponse();
